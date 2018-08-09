db9b056aeb96d1edffc856b6cb8d49a0a14c07c0
João Batista Neto <neto.joaobatista@gmail.com>
2018-08-09 16:15:02 -0300

    Evita que campos nulos sejam enviados

--- src/Rede/SerializeTrait.php
+++ src/Rede/SerializeTrait.php
@@ -9,6 +9,8 @@ trait SerializeTrait
      */
     public function jsonSerialize()
     {
-        return array_filter(get_object_vars($this));
+        return array_filter(get_object_vars($this), function($v) {
+            return $v !== null;
+        });
     }
 }

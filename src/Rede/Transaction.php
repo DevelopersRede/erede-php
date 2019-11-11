cbfb4546203e71e5572231c889ef0799a246ee72
Filipe Augusto Magalhães <filipeaugusto@vipcommerce.com.br>
2019-11-11 09:39:16 -0300

    setAmout erro parse int value

--- src/Rede/Transaction.php
+++ src/Rede/Transaction.php
@@ -517,7 +517,7 @@ class Transaction implements RedeSerializable, RedeUnserializable
      */
     public function setAmount($amount)
     {
-        $this->amount = (int)($amount * 100);
+        $this->amount = ceil($amount * 100);
         return $this;
     }
 

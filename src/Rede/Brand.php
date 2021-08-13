60b9d7498df4bff38d05f41d9d4939c47cc3d6b4
Netto Passaro <netto@vpsistemas.com.br>
2021-08-13 18:30:09 -0300

    Adicionando o campo authorizationCode na classe Brand

--- src/Rede/Brand.php
+++ src/Rede/Brand.php
@@ -22,6 +22,11 @@ class Brand
     private $returnMessage;
 
     /**
+     * @var string
+     */
+    private $authorizationCode;
+
+    /**
      * @return string
      */
     public function getName(): string
@@ -78,5 +83,15 @@ class Brand
         return $this;
     }
 
+    /**
+     * @param string $authorizationCode
+     *
+     * @return Brand
+     */
+    public function setAuthorizationCode(string $authorizationCode): Brand
+    {
+        $this->authorizationCode = $authorizationCode;
+        return $this;
+    }
 
 }

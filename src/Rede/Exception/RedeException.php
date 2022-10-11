7d70bfa472e46b9f50f05dd7acde08be3eabbe5c
Pablo Sanches <sanches.webmaster@gmail.com>
2022-10-11 15:32:54 -0300

    Adicionando o rawbody da resposta na exception de transação

--- src/Rede/Exception/RedeException.php
+++ src/Rede/Exception/RedeException.php
@@ -6,4 +6,20 @@ use RuntimeException;
 
 class RedeException extends RuntimeException
 {
+    private string $response;
+
+    public function __construct(
+        string $message = "",
+        int $code = 0,
+        ?Throwable $previous = null,
+        string $response
+    ) {
+        parent::__construct($message, $code, $previous);
+        $this->response = $response;
+    }
+
+    public function getResponse(): string
+    {
+        return $this->response;
+    }
 }

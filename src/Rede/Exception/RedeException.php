fe6476425c6dfa2ef148ffd8a51f07a8afbd9c0c
Pablo Sanches <sanches.webmaster@gmail.com>
2022-10-11 15:35:13 -0300

    Fazendo com que a response seja opcional na exception

--- src/Rede/Exception/RedeException.php
+++ src/Rede/Exception/RedeException.php
@@ -3,22 +3,23 @@
 namespace Rede\Exception;
 
 use RuntimeException;
+use Throwable;
 
 class RedeException extends RuntimeException
 {
-    private string $response;
+    private ?string $response = null;
 
     public function __construct(
         string $message = "",
         int $code = 0,
         ?Throwable $previous = null,
-        string $response
+        ?string $response = null
     ) {
         parent::__construct($message, $code, $previous);
         $this->response = $response;
     }
 
-    public function getResponse(): string
+    public function getResponse(): ?string
     {
         return $this->response;
     }
